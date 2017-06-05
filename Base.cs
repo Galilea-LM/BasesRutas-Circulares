01de210122775b4c8cd885b64ac4532950e8e043
Galilea-LM <glopez1@ucol.mx>
2017-06-04 23:22:22 -0500

    Add files via upload

---
+++ Base.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3Circulares_Bases_y_Rutas
+{
+    class Base
+    {
+        public string nombreBase { get; set; }
+        public DateTime minutos { get; set; }
+        public Base siguiente { get; set; }
+        public Base anterior { get; set; }
+
+        public Base(string nombreBase,DateTime minutos)
+        {
+            this.nombreBase = nombreBase;
+            this.minutos = minutos;
+        }
+
+        public override string ToString()
+        {
+            string regresa = "";
+            regresa += "nombre: " + nombreBase + " Minutos: " + minutos;
+            return regresa;
+                
+        }
+    }
+}
---
+++ Ruta.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3Circulares_Bases_y_Rutas
+{
+    class Ruta
+    {
+        private Base inicio;
+        public Ruta()
+        {
+            inicio = null;
+        }
+
+        public void agregarBase(Base nueva)
+        {
+           
+            bool Agregar = true;
+            if(inicio== null)
+            {
+                inicio = nueva;
+                nueva.siguiente = inicio;
+            }
+            else
+            {
+                Base temporal = inicio;
+                while(temporal.siguiente!= inicio && Agregar != false)
+                {
+                    if(temporal.nombreBase == nueva.nombreBase)
+                    {
+                        Agregar = false;
+                    }
+                }
+                if(Agregar==true && temporal.nombreBase != nueva.nombreBase)
+                {
+                    temporal.siguiente = nueva;
+                    nueva.siguiente = inicio;
+                }
+            }
+            
+        }
+
+        public void agregarInicio(Base nueva)
+        {
+            Base temporal = inicio;
+            bool Agregar = true;
+
+            if (inicio == null)
+            {
+                inicio = nueva;
+                nueva.siguiente = inicio;
+            }
+
+            else
+            {
+                while (temporal.siguiente != inicio && Agregar != false)
+                {
+
+                    if (temporal.nombreBase == nueva.nombreBase)
+                    {
+                        Agregar = false;
+                    }
+                    temporal = temporal.siguiente;
+                }
+                if (temporal.nombreBase != nueva.nombreBase && Agregar == true)
+
+                {
+                    nueva.siguiente = inicio;
+                    inicio = nueva;
+                    temporal.siguiente = inicio;
+                }
+            }
+        }
+
+       
+
+        public void eliminar(string nombreBase)
+        {
+            Base temporal = inicio;
+            if (temporal.nombreBase == nombreBase)
+            {
+                if (temporal.siguiente == inicio)
+                {
+                    temporal = null;
+                    inicio = temporal;
+                }
+
+                else
+                {
+                    while (temporal != inicio)
+                    {
+                        temporal = temporal.siguiente;
+                    }                 
+                    temporal.siguiente = inicio.siguiente;
+                    inicio = inicio.siguiente;
+                }
+            }
+
+        }
+
+        public void EliminarInicio()
+        {
+            Base temporal = inicio;
+            if (temporal.siguiente == inicio)
+                inicio = null;
+            else
+            {
+                while (temporal.siguiente != inicio)
+                {
+                    temporal = temporal.siguiente;
+                    temporal.siguiente = inicio.siguiente;
+                    inicio = inicio.siguiente;
+                }          
+            }
+        }
+
+        public Base buscarBase(string nombreBase)
+
+        {
+
+            Base temporal = inicio;
+
+            while (temporal.siguiente != inicio && temporal.nombreBase != nombreBase)
+
+            {
+
+                temporal = temporal.siguiente;
+
+            }
+
+            return temporal;
+
+        }
+    }
+}
